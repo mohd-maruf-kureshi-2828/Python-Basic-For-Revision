c6091945b01d5834c70ce4fe025e05c368655f40
mohd-maruf-kureshi-2828 <mohamedmarufkureshikureshi@gmail.com>
2026-03-25 17:02:05 +0530

    Encapsolation

--- OOPS/encpsulation.py
+++ OOPS/encpsulation.py
@@ -12,4 +12,33 @@ class Car:
 
     
 my_sec=Car('Toyata','Fortuner')
-print(my_sec.get_brand())
+# print(my_sec.get_brand())
+
+
+# second example for bank account
+
+class BankAccount:
+    def __init__(self,balance):
+        self.__balance=balance
+
+    def deposit(self,amount):
+        if amount > 0:
+            self.__balance += amount
+
+    def withdraw(self,amount):
+        if 0 < amount <= self.__balance:
+            self.__balance -= amount
+        else:
+            print('invalid withdraw')
+
+    def getBalace(self):
+        return self.__balance
+    
+kumar=BankAccount(1200)
+print('after withdraw',kumar.getBalace())
+kumar.withdraw(500)
+print('before withdraw',kumar.getBalace())
+kumar.deposit(500)
+print('before deposit',kumar.getBalace())
+
+# kumar.deposit(100)
